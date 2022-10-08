0d1a1243c8e6524aa7aea7332ba350053f263f1d
Taras <tarashuledza14@gmail.com>
2022-10-08 14:48:39 +0300

    lab 3.2

---
+++ Lab_03.2/Lab_03.2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	double x;
+	double a;
+	double b;
+	double c;
+	double F;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	cout << "x = "; cin >> x;
+
+	if (c < 0 && b != 0)
+		F = a * x * x + b * b * x;
+	else
+		if (c > 0 && b == 0)
+			F = (x + a) / (x + c);
+		else
+			F = x / c;
+	cout << "F = " << F << endl;
+
+	cin.get();
+	return 0;
+}
